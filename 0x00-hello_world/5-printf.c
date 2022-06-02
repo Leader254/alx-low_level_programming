f0135e7a20656256028365919b1915b6bf97028a
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2022-06-02 08:00:52 -0700

    Write a C program that prints exactly

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
+
+
+
 /**
  *
  *  * main - displays text
  *
- **/
-int main(void)
+ *   * @void: void
+ *
+ *    *
+ *
+ *     * Description: displays text
+ *
+ *      * Return: 0
+ *
+ *       */
+
+int	main(void)
+
 {
-	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
-        return (0);
+
+		printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+			return (0);
+
 }

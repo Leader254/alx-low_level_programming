7f7610272fdb1c55eed44e8082aa1d1a56ed6311
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2022-06-02 13:20:28 -0700

    C program that prints exactly

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,21 +7,12 @@
  *  * main - displays text
  *
  *   * @void: void
- *
- *    *
- *
  *     * Description: displays text
- *
  *      * Return: 0
- *
  *       */
 
 int	main(void)
-
 {
-
 		printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
-
-			return (0);
-
+		return (0);
 }

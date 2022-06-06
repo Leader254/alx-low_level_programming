447ec7e6580339e023c7c8fb46943e53b3408d5e
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2022-06-06 01:04:26 -0700

    program will assign a random number to the variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
-#include<stdio. h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function goes there */
@@ -12,12 +12,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if(n>0){
-		printf("%d is positive\n")
+		printf("%d is positive\n");
 	}
 	else if(n==0){
-		printf("%d is zero\n")
+		printf("%d is zero\n");
 	}
 	else 
-		printf("%d is negative\n")
+		printf("%d is negative\n");
 			return (0);
+
 }

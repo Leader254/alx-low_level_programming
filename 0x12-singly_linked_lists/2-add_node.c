6980d08eee8d9a10a1b09e772778e31816ba0fb5
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2022-07-14 06:11:22 -0700

    adds a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,47 +1,25 @@
 #include "lists.h"
-
 #include <string.h>
 
 /**
- *
- *  *add_node - a function that adds a new node at the beginning of a list_t list
- *
- *   *@head: A double pointer that points to the address of what's passed in
- *
- *    *@str: The string to be copied by strdup
- *
- *     *Return: The pointer to the next node
- *
- *      */
+ * add_node - a function that adds a new node at the beginning of a list_t list
+ * @head: A double pointer that points to the address of what's passed in
+ * @str: The string to be copied by strdu
+ * Return: The pointer to the next node
+ */
 
 list_t *add_node(list_t **head, const char *str)
-
 {
+	list_t *new;
+	int i;
 
-		list_t *new;
-
-			int i;
-
-
-
-				new = malloc(sizeof(list_t));
-
-					if (new == NULL)
-
-								return (NULL);
-
-						for (i = 0; str[i] != '\0'; i++)
-
-									;
-
-							new->len = i;
-
-								new->str = strdup(str);
-
-									new->next = *head;
-
-										*head = new;
-
-											return (*head);
-
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++);
+	new->len = i;
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
 }

c11c2b99c433120df7e237a42bcdc9be4c44ec21
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2022-07-14 06:13:25 -0700

    adds a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,7 +16,8 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 	new->len = i;
 	new->str = strdup(str);
 	new->next = *head;
